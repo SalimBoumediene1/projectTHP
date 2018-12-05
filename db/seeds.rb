46dfa4057b9beca1aa0bf9afd758bf2fe0d59527
Redcozmo <redcozmo@hotmail.com>
2018-12-05 16:36:40 +0100

    modify seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,20 +11,17 @@ require 'nokogiri'
 require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://www.lyon-france.com/Je-decouvre-Lyon/activites-loisirs-et-bien-etre/Parcs-jardins-et-lieux-de-balade"))
-@taburl = []
-@tabtitle = []
+@tab_url = []
+@tab_title = []
+@tab_description = []
 
-doc.css('.header-text-one a @title').each do |title| @tabtitle << title.text end
-doc.css('.list_offres .card-list-horizontal-content .col-xs-12 a img @src').each do |url| @taburl << url.text end
-@tablatitude = [45.777411, 45.782929, 45.773373, 45.775568, 49.252947, 45.780971]
-@tablongitude = [4.855225, 4.852306, 4.854822, 4.858500, 4.030319, 4.832410]
+doc.css('.header-text-one a @title').each do |elem| @tab_title << elem.text end
+doc.css('.list_offres .card-list-horizontal-content .col-xs-12 a img @src').each do |elem| @tab_url << elem.text end
+doc.css('.list_offres .card-list-horizontal-content .col-xs-12 a @description').each do |elem| @tab_description << elem.text end
+
+@tab_latitude = [45.777411, 45.782929, 45.773373, 45.775568, 49.252947, 45.780971, 45.798634, 45.741780, 45.762819, 45.721260, 45.785825]
+@tab_longitude = [4.855225, 4.852306, 4.854822, 4.858500, 4.030319, 4.832410, 4.940681, 4.808979, 4.823683, 4.828134, 4.730715]
 
 10.times do |y|
-    @places = Place.create(name: @tabtitle[y], image_url: @taburl[y], latitude: @tablatitude[y], longitude: @tablongitude[y])
+    @places = Place.create(name: @tab_title[y], description: @tab_description[y], image_url: @tab_url[y], latitude: @tab_latitude[y], longitude: @tab_longitude[y])
 end
-
-# Place.create!([
-# { "name": "Buckingham Palace", "latitude": "51.501564","longitude": "-0.141944"},
-# { "name": "Westminster Abbey", "latitude": "51.499581", "longitude": "-0.127309"},
-# { "name": "Big Ben", "latitude": "51.500792", "longitude": "-0.124613"}
-# ])
