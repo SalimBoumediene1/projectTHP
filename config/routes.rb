03617630dd25a1cfe1c3ad55b9402a94101fd908
Salim <salimboumediene@gmail.com>
2018-12-06 18:03:32 +0100

    template v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  resources :places
-  root 'home#index'
-  devise_for :users
+  root 'statics#home'
+  get 'statics/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20181205142547_add_column_to_places.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnToPlaces < ActiveRecord::Migration[5.2]
-  def change
-    add_column :places, :description, :text
-    add_column :places, :image_url, :string
-  end
-end
